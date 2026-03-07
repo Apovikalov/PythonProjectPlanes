1b93de41cfd5d39c16f7c7b7b1ff5f9e4d683525
Андрей Повикалов <apovikalov@bk.ru>
2026-03-07 11:08:54 +0600

    ignored

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,13 @@
 import pytest
 
 import src.api
-from src.api import Parser, APIAdapter
-from src.json_saver import JSONSaver
+from src.api import APIAdapter
+
 
 @pytest.fixture
 def api_adapt():
     return APIAdapter()
+
 
 def test_get_aeroplanes(api_adapt):
     assert src.api.APIAdapter.get_aeroplanes(api_adapt, "Canada") is None
